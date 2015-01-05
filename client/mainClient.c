6d543cf0a09765877f0056560563eb14583c8ab5
guigui33 <guillaumecantal@gmail.com>
2015-01-05 20:39:46 +0100

    modif main

--- client/mainClient.c
+++ client/mainClient.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "client.h"
 
 int main()
 {
     char *authentification=NULL;
     char *message=NULL;
-    char *msgConnexion=NULL;
-    size_t fin=0;
+    int continuer=1;
+    char msgConnexion[255];
+    msgConnexion[0]='\0'; // on initialise le premier caractère de msg connexion à \0
 
-    if(Initialisation("localhost") != 1)
+   /* if(Initialisation("localhost") != 1)
     {
         printf("Erreur d'initialisation\n");
         return 1;
     }
-
+*/
     do
     {
-//        menuConnex(msgConnexion);
+        menuConnex(msgConnexion);
        // Emission(msgConnexion);
 //        Reception(msg);
 //        menu();
+        continuer=strncmp(msgConnexion,"quitter",7);
     }
-    while(!fin);
-
+    while(continuer);
+   // EmissionBinaire("quitter",7);
     Terminaison();
 
     return EXIT_SUCCESS;
@@ -40,9 +43,9 @@ mode de passe dans une chaîne de caractères
 
  reception(msg);// msg retour serveur
 
- si msg="identif OK" alors /*l'utilisateur peut accéder au menu */
+ si msg="identif OK" alors l'utilisateur peut accéder au menu
 
- /*Menu(); /* dans la fonction menu l'utilisateur interagira avec le serveur
+ Menu();  dans la fonction menu l'utilisateur interagira avec le serveur
 
  FinSI
 
